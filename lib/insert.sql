8aab0bfb23045ee73d86c00e31b27dc70f834c47
William German <william.j.german2@gmail.com>
2022-12-15 16:59:29 -0800

    commit tree a3e754efbde26af04eda26dbf344274677f9e169

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'chocolate', 'asshole', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'chocolate', 'asshole', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'chocolate', 'asshole', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'chocolate', 'asshole', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'chocolate', 'asshole', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'F', 'chocolate', 'asshole', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'M', 'chocolate', 'asshole', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, Null, null, null, null, null);
